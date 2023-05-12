bd5b503506581e378b7fae47b3952292e29caac1
Eko Sudarwanto <20102097@ittelkom-pwt.ac.id>
2023-05-12 23:55:05 +0700

    Initial commit

---
+++ app/src/main/java/com/example/modul6_20102143/DBModel.kt
@@ -0,0 +1,8 @@
+package com.example.modul6_20102143
+
+data class DBModel  (
+    var email: String = "",
+    var pass: String = "",
+    var username: String = "",
+    var fullname: String = ""
+)
